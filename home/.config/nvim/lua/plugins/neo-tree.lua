9a45975806baf56c45bc465c2b286d417c96b13f
MonsieurTib <tibo.lecoq@gmail.com>
2026-04-06 16:27:09 +0200

    lazyload NeoTree

--- home/.config/nvim/lua/plugins/neo-tree.lua
+++ home/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,14 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
+	cmd = "Neotree",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	config = function()
-		vim.keymap.set("n", "<leader>ef", "<CMD>Neotree filesystem focus reveal left<CR>", { desc = "Explorer Focus" })
-		vim.keymap.set("n", "<leader>et", "<CMD>Neotree filesystem toggle reveal left<CR>", { desc = "Explorer Toggle" })
-	end,
+	keys = {
+		{ "<leader>ef", "<CMD>Neotree filesystem focus reveal left<CR>", desc = "Explorer Focus" },
+		{ "<leader>et", "<CMD>Neotree filesystem toggle reveal left<CR>", desc = "Explorer Toggle" },
+	},
 }
